488cbd9c1901026d8c84bb1525ab42bd7e1a372c
1jjanata <42749702+1jjanata@users.noreply.github.com>
2018-11-15 13:58:48 -0600

    Add files via upload

--- tracker.js
+++ tracker.js
@@ -1,11 +1,31 @@
 //Type your code in this function
 function Tracker(){
 
+  
+  
   let seeds =[];
   let percent = [];
 
   //implement all six functions below
 
+function numOfSeeds(list, length){
+  let found = 0;
+  for(let i=0; i < list.length; i++;){
+    if(list[i]=== value){
+      found++;
+    }
 
-function numOfSeeds()
+}
+return found;
+}
+
+function addSeed("s"){
+  list.push("s")
+}
+
+
+
+
+
+function reportHealth()
 }
