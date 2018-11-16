3d29ecbdff11031bb3f1c158422949db6ba2a24b
1jjanata <42749702+1jjanata@users.noreply.github.com>
2018-11-16 13:54:54 -0600

    Add files via upload

--- file.js
+++ file.js
@@ -2,16 +2,20 @@
 function TorrentFile(){
 
 //first function
-
+function numberOfPackets(fileSize){
+    return fileSize/50
+    } 
 
 
 
 
 //second function
-
-
-
-
+function buildTorrentPackeyArray(numOfPackets){
+    let list;
+    let numOfPackets;
+    for (let i=0; i < numOfPackets, i++;){
+        list [i]= 1
+    }
 }
 
 //main test methods
--- tracker.js
+++ tracker.js
@@ -1,31 +1,37 @@
 //Type your code in this function
 function Tracker(){
 
-  
-  
   let seeds =[];
   let percent = [];
 
-  //implement all six functions below
+  //implement all five functions below
 
-function numOfSeeds(list, length){
-  let found = 0;
-  for(let i=0; i < list.length; i++;){
-    if(list[i]=== value){
-      found++;
-    }
-
-}
-return found;
+  function numOfSeeds(list,value)
+ let found = 0
+ for(let i=0; i<list.length; i++){
+   if(list[i] === value)
+   found++
+ return found
+ }
 }
 
 function addSeed("s"){
-  list.push("s")
+  seeds.push(name);
+
 }
 
+function removeSeed(seeds, name){
+  let index = 0;
+  for (let i = 0; i < seeds.length; i++){
+    if(list[i] === name){
+      
 
 
 
 
-function reportHealth()
-}
+function reportHealth(){
+
+
+
+
+
