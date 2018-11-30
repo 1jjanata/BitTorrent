3bbfa1bde9d084a56bf974cb4a4297ba426cb6f4
1jjanata <42749702+1jjanata@users.noreply.github.com>
2018-11-30 13:55:13 -0600

    Add files via upload

--- user.js
+++ user.js
@@ -3,8 +3,42 @@ function User(tracker, torrent){
 
   let myTracker = tracker;
   let myTorrent = torrent;
+  let state= "seed"
 
-  //Type all of your code in here.
+  let downloadSpeed= 0; 
+
+  let uploadSpeed = 0;
+ //Type all of your code in here.
+
+}
+function startDownload(){
+
+}
+function updateTracker(){
+  function Tracker(){
+    function average(list){
+      let sum = 0;
+      for(let i = 0; i< list.length; i++)
+      sum = sum + list [i];
+    }
+return sum / list.length
+
+  }
+  function checkProgress(){
+    function TorrentFile(){
+      function average(list){
+        let sum = 0;
+        for(let i = 0; i < list.length; i++){
+          sum = sum + list
+        }
+      }
+    }
+  }
+  function leech(){
+
+
+  }
+  function seed(){
 
 
 
